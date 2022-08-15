735226a417fd8646a83538bbe33092b8dba72d15
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-08-15 17:51:56 +0200

    Loader

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -6,11 +6,13 @@ import PostCard from "../elements/PostCard";
 
 export default function Home() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isReqDone, setIsReqDone] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await requests.posts.getAllPosts();
       setPosts(res);
+      setIsReqDone(true);
     };
 
     fetchPosts();
@@ -25,10 +27,21 @@ export default function Home() {
           posts.map((post) => {
             return <PostCard key={post.id} {...post} />;
           })
-        ) : (
+        ) : isReqDone ? (
           <h1 className="home__no_posts">
             No posts at the moment... <Link to={"/create-post"}>Post Here</Link>
           </h1>
+        ) : (
+          <div className="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         )}
       </div>
     </div>
